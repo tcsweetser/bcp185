59af7c6693ad227d0fb73ed6f32ee107ef09197b
Terry Sweetser <tcs@apnic.net>
2024-02-16 19:24:45 +1000

    Adding minimal docs +1

--- load.sql
+++ load.sql
@@ -1,1 +1,11 @@
 copy routes from stdin;
+
+
+--- try these:
+
+--- copy routes from '/tmp/data' (delimiter('|'));
+--- copy rpki_signed_routes from '/tmp/roas' with csv header ;
+--- copy allocated from '/tmp/assignments' (delimiter('|'));
+
+---
+
